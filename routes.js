c2536d9b01e45c0648a18caba7f577705832c4a9
xorana <sidtheseer@gmail.com>
2016-11-01 18:19:16 +1100

    Stuff I did today

--- routes.js
+++ routes.js
@@ -1,7 +1,12 @@
+var express = require('express');
 var page = require('./page');
 
 module.exports = function(app){
     app.get('/', function(req, res){
         res.send(page.homepage());
     });
-}
+
+    app.use('/static', express.static('static'));
+    app.use('/fonts', express.static('fonts'));
+    app.use('/images', express.static('images'))
+};
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var logger = require('morgan');
 var jade = require('jade');
-// var template = jade.compileFile(__dirname + '/homepage.jade');
-// var template2 = jade.compileFile(__dirname + '/test.jade');
 
+// Start the Express app
 var app = express();
 
 // Routes
 var routes = require('./routes')(app)
 
+// Start listening for requests
 app.listen(process.env.PORT || 3000, function(){
     console.log('Listening on http://localhost:' + (process.env.PORT || 3000));
 });
